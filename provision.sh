44a289bf4ccf087f387475a70bf0290434f0e243
Renã Carneiro Pedroso <rena.pedroso@somosyoung.com.br>
2023-03-31 16:05:00 -0300

    adicionando conteúdo

---
+++ provision.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# vagrant ssh -> comando para acessar a maquina
+# o diretório /vagrant sincroniza todos os dados que estao no diretório principal
+# vagrant up -> inicia a maquina
+# vagrant halt -> pausa a maquina
